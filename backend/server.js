c60ad8337bbdfefa863696b4be540912361d65c8
TMPO <nunezp20@gmail.com>
2022-09-19 19:50:59 -0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile('home.html', { root: './frontend/public' });
+    console.log("Home page succesfully loaded");
+  });
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000...");
+});
+
+app.use(express.static('./frontend'));
